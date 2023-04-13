dcff030b1f9414c604e216b70cc09f8a937acdaf
Maruf Hasan Anik <111722365+Skippersquareroot100@users.noreply.github.com>
2023-04-13 11:28:10 +0600

    Add files via upload

---
+++ DUnW.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int Vertices,Edge,From,To;
+
+  cout<<"Enter Vertices:";
+  cin>>Vertices;
+  cout<<"Enter Edge:";
+  cin>>Edge;
+
+
+  int arr[Vertices][Vertices];
+
+
+
+  for(int i=0;i<Vertices;i++)
+  {
+      for(int j=0;j<Vertices;j++)
+  {
+       arr[i][j]=0;
+  }
+  }
+   
+
+  cout<<"Plot Your Graph:\n";
+
+  for(int i=0;i<Edge;i++)
+  {
+     cin>>From>>To;
+     arr[From][To]=1;
+     
+
+  }
+  
+cout<<"Adjacency Matrix:\n";
+    for(int i=0;i<Vertices;i++)
+  {
+      for(int j=0;j<Vertices;j++)
+  {
+       cout<<arr[i][j]<<" ";
+  }
+  cout<<endl;
+  }
+
+}
---
+++ UndW.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int Vertices,Edge,From,To,Weigth;
+  cout<<"Enter Vertices:";
+  cin>>Vertices;
+  cout<<"Enter Edge:";
+  cin>>Edge;
+  int arr[Vertices][Vertices];
+  for(int i=0;i<Vertices;i++)
+  {
+      for(int j=0;j<Vertices;j++)
+  {
+       arr[i][j]=0;
+  }
+  }
+  cout<<"Plot Your Graph:\n";
+
+  for(int i=0;i<Edge;i++)
+  {
+     cin>>From>>To>>Weigth;
+     arr[From][To]=Weigth;
+     arr[To][From]=Weigth;
+
+  }
+cout<<"Adjacency Matrix:\n";
+    for(int i=0;i<Vertices;i++)
+  {
+      for(int j=0;j<Vertices;j++)
+  {
+       cout<<arr[i][j]<<" ";
+  }
+  cout<<endl;
+  }
+}
